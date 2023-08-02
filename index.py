08e98b1c0efc26107d422efc5adf01d36f060680
leo <leoleitao1227@gmail.com>
2023-08-01 22:42:03 -0300

    remap data

--- index.py
+++ index.py
@@ -1,3 +1,17 @@
+def intData(inteiro_data):
+    from datetime import datetime
+
+    # Convertendo o inteiro em uma string no formato "AAAA-MM-DD"
+    data_string = str(inteiro_data)
+
+    # Criando um objeto datetime a partir da string formatada
+    data_formatada = datetime.strptime(data_string, '%Y%m%d')
+
+    data_formatada_saida = data_formatada.strftime('%d/%m/%Y')
+
+    return(data_formatada_saida)
+
+
 vetor = []
 cont = 0
 
@@ -13,7 +27,7 @@ with open('extrato.txt', 'r') as arquivo:
             vetor_final = a.split(";")
 
             if(vetor_final[5] != 'C' and vetor_final[3]!= 'SALDO DIA'):    
-                print(vetor_final[1],vetor_final[3],vetor_final[4])   
+                print(intData(vetor_final[1]),vetor_final[3],vetor_final[4])   
 
 
     vetor_final.clear()
